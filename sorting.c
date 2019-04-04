0019990cb7b8a99e4bba76fba09e593a55225a97
colincassens <ccassens@purdue.edu>
2019-04-03 22:31:18 -0400

    Merge Working

--- sorting.c
+++ sorting.c
@@ -13,7 +13,6 @@ void put_together(long * Array, int lb, int up, int mid);
 /// Functions
 ///
 
-
 //MERGE SORT Not Working
 void Merge_Sort(long * Array, int Size)
 {
@@ -25,7 +24,7 @@ void merge_split(long * Array, int lb, int ub)
     if(lb >= ub){
         return;
     }
-    int mid = lb+ub/2;
+    int mid = (lb+ub)/2;
     merge_split(Array, lb, mid);
     merge_split(Array, mid+1, ub);
     put_together(Array, lb, ub, mid);
@@ -34,33 +33,33 @@ void merge_split(long * Array, int lb, int ub)
 
 void put_together(long * Array, int lb, int ub, int mid)
 {
-    long * left = calloc(mid-lb+1, sizeof(long));
-    long * right = calloc(ub-mid, sizeof(long));
-    memcpy(&left[0], &Array[lb], mid-lb+1);
-    memcpy(&right[0], &Array[mid+1], ub-mid);
+    long * left = malloc((mid-lb+1) * sizeof(long));
+    long * right = malloc((ub-mid) *sizeof(long));
+    memcpy(&left[0], &Array[lb], sizeof(long) * (mid-lb+1));
+    memcpy(&right[0], &Array[mid+1], sizeof(long)*(ub-mid));
     int i = lb;
     int j = mid + 1;
     int k;
     for(k = lb; k<= ub; k++){
         //Neither are finished
         if(i <= mid && j <= ub){
-            if(left[i] <= right[j]){
-                Array[k] = left[i];
+            if(left[i-lb] <= right[j-(mid+1)]){
+                Array[k] = left[i-lb];
                 i++;
             }
             else{
-                Array[k] = right[j];
+                Array[k] = right[j-(mid+1)];
                 j++;
             }
         }
         //Left array is finished
         else if(i > mid && j <= ub){
-            Array[k] = right[j];
+            Array[k] = right[j-(mid+1)];
             j++;
         }
         //Right array is finished
         else if(i <= mid && j > ub) {
-            Array[k] = left[i];
+            Array[k] = left[i-lb];
             i++;
         }
     }
